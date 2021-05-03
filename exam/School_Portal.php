898a33fa0fe8a7594de77fd27fc9d1a206d9737c
stupidsud <41414519+stupidsud@users.noreply.github.com>
2021-05-04 01:00:09 +0530

    Update School_Portal.php

--- exam/School_Portal.php
+++ exam/School_Portal.php
@@ -23,7 +23,7 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<meta name="view" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>Volunteer Login</title>
 <link  rel="stylesheet" href="css/bootstrap.min.css"/>
